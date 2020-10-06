17561d69fd6b976006a598ea347e6bd27592e00c
ryanream <ryan.a.ream@gmail.com>
2020-10-06 10:36:33 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  // life cycle method
+  render() {
+    
+    return (
+      <div className="App">
+        <h1>Ryan.</h1>
+      </div> 
+    );
+  }
+
+
+}
+
+export default App;
