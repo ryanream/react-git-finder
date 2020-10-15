c687ec91a4db45bc034e38d9fb5fddedad902402
ryanream <ryan.a.ream@gmail.com>
2020-10-15 12:40:05 -0700

    add comment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// need more commenting
+// need more comments throughout
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
